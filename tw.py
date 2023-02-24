4c55aef8eb2ab3ac36621c9c27b89319aaa90781
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-02-24 21:20:29 +0000

    Update tw.py

--- tw.py
+++ tw.py
@@ -2,14 +2,8 @@ import streamlit as st
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 from textblob import TextBlob
-import geopy
-import geopy.exc
 from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
-import plotly.express as px
-
-geolocator = Nominatim(user_agent="streamlit_app")
-geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+import folium
 
 def get_tweets(query, limit):
     tweets = []
@@ -26,14 +20,9 @@ def get_sentiment(tweet):
     return 'positive' if analysis.sentiment.polarity >= 0 else 'negative'
 
 def get_country(location):
-    if location is not None:
-        try:
-            country = geolocator.reverse(location, exactly_one=True).raw['address']['country']
-        except (geopy.exc.GeocoderTimedOut, geopy.exc.GeocoderServiceError, KeyError):
-            country = None
-    else:
-        country = None
-    return country
+    geolocator = Nominatim(user_agent="sentiment_analysis")
+    location = geolocator.geocode(location, timeout=10)
+    return location.address.split(",")[-1].strip()
 
 st.title("Twitter Sentiment Analysis")
 
@@ -49,9 +38,36 @@ if st.button("Search"):
     st.write(f"Found {len(tweets)} tweets.")
     st.write(f"Positive tweets: {sentiment_counts['positive']}")
     st.write(f"Negative tweets: {sentiment_counts['negative']}")
-    country_sentiment_counts = df.groupby('Country')['Sentiment'].value_counts().unstack(fill_value=0)
-    country_sentiment_counts['Total'] = country_sentiment_counts.sum(axis=1)
-    country_sentiment_counts = country_sentiment_counts.reset_index()
-    fig = px.choropleth(country_sentiment_counts, locations="Country", color="Total", 
-                        hover_name="Country", projection="natural earth")
-    st.plotly_chart(fig)
+    st.bar_chart(sentiment_counts)
+
+    # Create a dataframe with the count of positive and negative tweets by country
+    country_counts = df.groupby(['Country', 'Sentiment']).size().reset_index(name='Count')
+    positive_counts = country_counts[country_counts['Sentiment'] == 'positive']
+    negative_counts = country_counts[country_counts['Sentiment'] == 'negative']
+
+    # Create a map with markers for positive and negative tweet counts
+    st.subheader("Positive and Negative Tweet Counts by Country")
+    map_center = [0, 0]
+    m = folium.Map(location=map_center, zoom_start=1)
+    for index, row in positive_counts.iterrows():
+        country = row['Country']
+        count = row['Count']
+        location = geolocator.geocode(country, timeout=10)
+        if location:
+            lat = location.latitude
+            lon = location.longitude
+            tooltip_text = f"{country}: {count} positive tweets"
+            folium.Marker(location=[lat, lon], tooltip=tooltip_text, icon=folium.Icon(color='green')).add_to(m)
+    for index, row in negative_counts.iterrows():
+        country = row['Country']
+        count = row['Count']
+        location = geolocator.geocode(country, timeout=10)
+        if location:
+            lat = location.latitude
+            lon = location.longitude
+            tooltip_text = f"{country}: {count} negative tweets"
+            folium.Marker(location=[lat, lon], tooltip=tooltip_text, icon=folium.Icon(color='red')).add_to(m)
+    folium_static(m)
+
+    st.write(df)
+
