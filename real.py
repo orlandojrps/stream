b34f9a58a07cd2a4fc72e8207dbe3584151c4633
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-03 06:56:09 +0000

    Update real.py

--- real.py
+++ real.py
@@ -1,15 +1,8 @@
 import streamlit as st
 import pandas as pd
-import time
 import folium
 from geopy.geocoders import Nominatim
-from streamlit.elements import image_proto
-from streamlit.proto.Image_pb2 import Image as ImageProto
-from PIL import Image
-from io import BytesIO
-import base64
-from streamlit.proto.Image_pb2 import Image as ImageProto
-
+import time
 
 def geocode_and_plot_addresses(df):
     geolocator = Nominatim(user_agent='user-my-application') # create a geolocator object
@@ -21,18 +14,27 @@ def geocode_and_plot_addresses(df):
     for address in addresses:
         location = geolocator.geocode(address)
         time.sleep(0) # add a 1.1-second interval between requests
-
+    
+    # calculate summary statistics
+    num_houses = df['Price'].count()
+    avg_price = df['Price'].mean()
+    lowest_price = df['Price'].min()
+    highest_price = df['Price'].max()
+    
     # create a table with the summary statistics
-    count = len(df)
-    mean_price = df['Price'].mean()
-    min_price = df['Price'].min()
-    max_price = df['Price'].max()
-    summary_table = pd.DataFrame({'': [f'<i class="fas fa-home"></i> Number of Houses:', 
-                                       f'<i class="fas fa-dollar-sign"></i> Average Price:', 
-                                       f'<i class="fas fa-arrow-down"></i> Lowest Price:', 
-                                       f'<i class="fas fa-arrow-up"></i> Highest Price:'],
-                                  'Value': [count, f'${mean_price:.2f}', f'${min_price:.2f}', f'${max_price:.2f}']})
-    summary_table = summary_table.set_index('')
+    summary_table = pd.DataFrame({
+        'Number of Houses': [num_houses],
+        'Average Price': [avg_price],
+        'Lowest Price': [lowest_price],
+        'Highest Price': [highest_price]
+    })
+    
+    # create a streamlit table and add it to the interface
+    st.table(summary_table.style.format({
+        'Average Price': '${:.2f}',
+        'Lowest Price': '${:.2f}',
+        'Highest Price': '${:.2f}'
+    }))
     
     # plot the coordinates on a map using Folium
     map_center = [51.897928, -8.470579] # center the map on Cork City
@@ -41,8 +43,6 @@ def geocode_and_plot_addresses(df):
         if row['Latitude'] and row['Longitude']:
             folium.Marker([row['Latitude'], row['Longitude']], popup=row['Address']).add_to(m)
 
-    # create a streamlit table and add it to the interface
-    st.table(summary_table.style.format({'Value': '{0}'}).format({'Value': lambda x: Markup(x)}))
     # add the map to the interface
     st.write(m._repr_html_(), unsafe_allow_html=True)
 
