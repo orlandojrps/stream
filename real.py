03c12879f6e1f8e4f0b52a8f0c3041f84b889fd2
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-03 06:17:07 +0000

    Update real.py

--- real.py
+++ real.py
@@ -5,8 +5,7 @@ from geopy.geocoders import Nominatim
 import time
 import pandas as pd
 
-# Função para plotar mapa
-
+ 
 def geocode_and_plot_addresses(df):
     geolocator = Nominatim(user_agent='user-my-application') # create a geolocator object
     addresses = df['Address'].tolist() # extract addresses from DataFrame 
@@ -18,27 +17,34 @@ def geocode_and_plot_addresses(df):
         location = geolocator.geocode(address)
         time.sleep(0) # add a 1.1-second interval between requests
 
-    
+    # create a table with the summary statistics
+    summary_table = df[['Price', 'Beds', 'Baths', 'Area']].describe()
+
     # plot the coordinates on a map using Folium
     map_center = [51.897928, -8.470579] # center the map on Cork City
     m = folium.Map(location=map_center, zoom_start=12)
     for i, row in df.iterrows():
         if row['Latitude'] and row['Longitude']:
             folium.Marker([row['Latitude'], row['Longitude']], popup=row['Address']).add_to(m)
-    return m
 
-# Carregar DataFrame
-#df2 = pd.read_csv('https://raw.githubusercontent.com/orlandojrps/stream/main/stage2_df.csv')
+    # create a streamlit table and add it to the interface
+    st.table(summary_table)
+
+    # add the map to the interface
+    st.write(m._repr_html_(), unsafe_allow_html=True)
+
+# Load the DataFrame
 df = pd.read_csv('https://raw.githubusercontent.com/orlandojrps/stream/main/df_final.csv')
 
-# Criar input para selecionar city_area
-city_area = st.selectbox('Selecione uma city_area', df['city_area'].unique())
+# Create input to select city_area
+city_area = st.selectbox('Select a city_area', df['city_area'].unique())
 
-# Filtrar DataFrame com base na city_area selecionada
+# Filter the DataFrame based on the selected city_area
 filtered_df = df[df['city_area'] == city_area]
 
-# Criar botão para plotar endereços no mapa
-if st.button('Plotar endereços no mapa'):
-    map = geocode_and_plot_addresses(filtered_df.head(9))
+# Create a button to plot addresses on the map
+if st.button('Plot addresses on the map'):
+    geocode_and_plot_addresses(filtered_df.head(9))
+
     # Adicionar mapa na interface
     st.write(map._repr_html_(), unsafe_allow_html=True)
