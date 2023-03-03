d5c71b86192372744b58c6a24a332c9924e4628e
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-03 04:05:29 +0000

    Update untitled51.py

--- untitled51.py
+++ untitled51.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1NN0kYs4riuXCjqeoKd40VZ66Lh1hQIFn
 """
 
-!pip install streamlit
+#!pip install streamlit
 
 import streamlit as st
 import pandas as pd
