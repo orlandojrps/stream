5b42bd5a2847dc6eb9f104510057fe937c806980
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-03 06:40:34 +0000

    Update real.py

--- real.py
+++ real.py
@@ -3,6 +3,7 @@ import pandas as pd
 import folium
 from geopy.geocoders import Nominatim
 import time
+import streamlit_components as stc
 
 def geocode_and_plot_addresses(df):
     geolocator = Nominatim(user_agent='user-my-application') # create a geolocator object
@@ -14,28 +15,24 @@ def geocode_and_plot_addresses(df):
     for address in addresses:
         location = geolocator.geocode(address)
         time.sleep(0) # add a 1.1-second interval between requests
-    
-    # calculate summary statistics
-    num_houses = df['Price'].count()
-    avg_price = df['Price'].mean()
-    lowest_price = df['Price'].min()
-    highest_price = df['Price'].max()
-    
+
     # create a table with the summary statistics
-    summary_table = pd.DataFrame({
-        'Number of Houses': [num_houses],
-        'Average Price': [avg_price],
-        'Lowest Price': [lowest_price],
-        'Highest Price': [highest_price]
-    })
-    
-    # create a streamlit table and add it to the interface
-    st.table(summary_table.style.format({
-        'Average Price': '${:.2f}',
-        'Lowest Price': '${:.2f}',
-        'Highest Price': '${:.2f}'
-    }))
-    
+    num_houses = df.shape[0]
+    mean_price = df['Price'].mean()
+    min_price = df['Price'].min()
+    max_price = df['Price'].max()
+
+    data = {
+        ' ': [stc.html('<i class="fas fa-home"></i>'), 
+              stc.html('<i class="fas fa-money-bill-wave"></i>'), 
+              stc.html('<i class="fas fa-arrow-down"></i>'), 
+              stc.html('<i class="fas fa-arrow-up"></i>')],
+        'Statistic': ['Number of Houses', 'Average Price', 'Lowest Price', 'Highest Price'],
+        'Value': [num_houses, f'${mean_price:,.2f}', f'${min_price:,.2f}', f'${max_price:,.2f}']
+    }
+
+    table = stc.html('<i class="fas fa-table"></i>') + st.table(data)
+
     # plot the coordinates on a map using Folium
     map_center = [51.897928, -8.470579] # center the map on Cork City
     m = folium.Map(location=map_center, zoom_start=12)
@@ -43,7 +40,14 @@ def geocode_and_plot_addresses(df):
         if row['Latitude'] and row['Longitude']:
             folium.Marker([row['Latitude'], row['Longitude']], popup=row['Address']).add_to(m)
 
-    # add the map to the interface
+    # add the table and map to the interface
+    stc.html('<hr>') # add a horizontal line
+    stc.html('<h2>Summary Statistics:</h2>')
+    stc.html('<br>')
+    stc.html('<br>')
+    st.write(table, unsafe_allow_html=True)
+    stc.html('<hr>') # add a horizontal line
+    stc.html('<h2>Map:</h2>')
     st.write(m._repr_html_(), unsafe_allow_html=True)
 
 # Load the DataFrame
@@ -58,3 +62,4 @@ filtered_df = df[df['city_area'] == city_area]
 # Create a button to plot addresses on the map
 if st.button('Plot addresses on the map'):
     geocode_and_plot_addresses(filtered_df)
+
