0a14381105365693bf0d73da7bd9e2e8f65b4fb1
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-05 19:36:11 +0000

    Update final.py

--- final.py
+++ final.py
@@ -224,6 +224,10 @@ with c2:
         #legend='bottom', 
         #use_container_width=True)
 
+map_url = 'https://www.google.com/maps/@51.9011024,-8.4941951,3a,75y,254.1h,90t/data=!3m7!1e1!3m5!1sAF1QipOz-BpF13JhlgTmgHuyBHQE7VnJv6uNE7-UUfL6!2e10!3e12!7i16384!8i8192'
+
+st.write(f'<iframe src="{map_url}" width="1000" height="500"></iframe>', unsafe_allow_html=True)
+
 # Row C
 st.markdown('### Line chart')
 st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
