0397bd615a058dffaf12d8003ddbb562d5d8d870
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-05 18:11:24 +0000

    Update final.py

--- final.py
+++ final.py
@@ -106,8 +106,8 @@ metric_col2 = f"<div style='font-size: 24px; font-weight: bold;'>Average Area (m
 col2.markdown(metric_col2, unsafe_allow_html=True)
 col2.metric("", f" {avg_area:,.2f}", " ")
 col1m, col2m = st.columns(2)
-metric_html = f"<div style='font-size: 18px; font-weight: bold;'>Min (m2):  {min_area:,.f}</div>"
-metric_html2 = f"<div style='font-size: 18px; font-weight: bold;'>Max (m2):  {max_area:,.f}</div>"
+metric_html = f"<div style='font-size: 18px; font-weight: bold;'>Min (m2):  {min_area:}</div>"
+metric_html2 = f"<div style='font-size: 18px; font-weight: bold;'>Max (m2):  {max_area:}</div>"
 
 col1m, col2m = st.columns(2)
 col2.markdown(metric_html + metric_html2, unsafe_allow_html=True)
