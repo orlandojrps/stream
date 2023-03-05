7f492d7ecd694f4a1a3fe8c7dfbdec3643099737
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-05 22:40:18 +0000

    Update final.py

--- final.py
+++ final.py
@@ -162,9 +162,9 @@ col4.markdown(metric_html + metric_html2, unsafe_allow_html=True)
 ##################################
 ###############################
 
-metric_col0 = f"<div style='font-size: 24px; font-weight: bold; color:#009FB2;'>Number Houses (Qty):</div>"
+metric_col0 = f"<div style='font-size: 24px; font-weight: bold; color:#000000;'>Number Houses (Qty):</div>"
 col0.markdown(metric_col0, unsafe_allow_html=True)
-col0.metric("", f"<font color='blue'>{n_houses}</font>", " ")
+col0.metric("", f" {n_houses:}", " ")
 #col1m, col2m = st.columns(2)
 #metric_html = f"<div style='font-size: 18px; font-weight: bold;'>Min (Qty):  {min_baths:}</div>"
 #metric_html2 = f"<div style='font-size: 18px; font-weight: bold;'>Max (Qty):  {max_baths:}</div>"
