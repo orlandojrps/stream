04cf5e1e0d18cebac2167ae657fbfccfbef412f3
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-06 16:19:40 +0000

    Update final.py

--- final.py
+++ final.py
@@ -548,7 +548,7 @@ features = np.array([pred_beds, pred_baths, pred_area, m2, avg_m2, avg_m2_area])
 #######features = np.array([pred_beds, pred_baths, pred_area, pred_area, pred_area, pred_area]).reshape(1, -1)
 prediction = predict(features)
 #c2.write('Your Suggested Price is:', 12)  
-formatted_prediction = "{:.2f}".format(prediction)
+formatted_prediction = "{:.2f}".format(prediction)/1000
 
 c1.markdown(f"<div style='font-size: 64px;'>{formatted_prediction}</div>", unsafe_allow_html=True)
 print(f"<div style='font-size: 64px;'>{formatted_prediction}</div>")
