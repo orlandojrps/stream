71268d28b8127e4fd7bf5fb92257f7840105d848
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-06 04:13:55 +0000

    Update final.py

--- final.py
+++ final.py
@@ -339,6 +339,9 @@ num_bins = 100
 
 
 
+
+
+
 #col3.metric("Humidity", "86%", "4%")
 #col5.metric("Humidity", "86%", "4%")
 #col6.metric("Humidity", "86%", "4%")
@@ -422,6 +425,34 @@ st.write(f'<a href="{map_url}" target="_blank">Click here to view map</a>', unsa
  
 
 
+    
+    
+from streamlit_modal import Modal
+
+import streamlit.components.v1 as components
+
+
+modal = Modal("Demo Modal")
+open_modal = st.button("Open")
+if open_modal:
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+
+        html_string = '''
+        <h1>HTML string in RED</h1>
+
+        <script language="javascript">
+          document.querySelector("h1").style.color = "red";
+        </script>
+        '''
+        components.html(html_string)
+
+        st.write("Some fancy text")
+        value = st.checkbox("Check me")
+        st.write(f"Checkbox checked: {value}")
 
 
 
