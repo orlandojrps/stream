a419dbe635875856ead76a2afab65d719105f178
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-06 04:34:19 +0000

    Update final.py

--- final.py
+++ final.py
@@ -426,24 +426,6 @@ st.write(f'<a href="{map_url}" target="_blank">Click here to view map</a>', unsa
 
 
     
-import folium
-from streamlit_folium import st_folium, folium_static
-    
-#m = folium.Map(location=[df.latitude.mean(), df.longitude.mean()], zoom_start=3, control_scale=True)
-
-#Loop through each row in the dataframe
-#for i,row in df.iterrows():
-    #Setup the content of the popup
-iframe = folium.IFrame('Well Name:' + str(row["Well Name"]))
-    
-    #Initialise the popup using the iframe
-popup = folium.Popup(iframe, min_width=300, max_width=300)
-    
-    #Add each row to the map
-folium.Marker(location=[row['latitude'],row['longitude']],popup = popup, c=row['Well Name']).add_to(m)
-
-st_data = st_folium(m, width=700)  
-    
     
     
 
