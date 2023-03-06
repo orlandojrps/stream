f7b4a43147ec46698314b017a562f7b8927110d2
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-06 01:33:55 +0000

    Update final.py

--- final.py
+++ final.py
@@ -233,10 +233,10 @@ sns.set_palette('husl')
 #sns.kdeplot(df_filtered['Price'], ax=col5, shade=True, common_norm=True)
 
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(5,5))
 sns.kdeplot(data=df_filtered, x="Price", ax=ax, shade=True)
 ax.set(xlabel='Price', ylabel='Density')
-st.pyplot(fig, width=200, height=200, use_container_width=False)
+st.pyplot(fig)
 
 
 
