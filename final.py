e8bfca8b4496f0dd9a0a8d9a09c71c09931ab329
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-06 15:15:18 +0000

    Update final.py

--- final.py
+++ final.py
@@ -538,8 +538,11 @@ priceArea = df_filtered_pred['city_area'].map(average_price)
 
 features = np.array([pred_beds, pred_baths, pred_area, pred_area, pred_area, pred_area]).reshape(1, -1)
     
+    
+teste = median_price_m2[0]
+    
 #features = np.array([pred_beds, pred_baths, pred_area, median_price_m2, mean_price_m2, average_price]).reshape(1, -1)
 #######features = np.array([pred_beds, pred_baths, pred_area, pred_area, pred_area, pred_area]).reshape(1, -1)
 prediction = predict(features)
 #c2.write('Your Suggested Price is:', 12)  
-c2.markdown(f"<div style='font-size: 64px;'>{median_price_m2}</div>", unsafe_allow_html=True)
+c2.markdown(f"<div style='font-size: 64px;'>{teste}</div>", unsafe_allow_html=True)
