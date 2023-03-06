58d9ff11f4e6e2f686fb8293d30e748443249550
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-06 01:54:02 +0000

    Update final.py

--- final.py
+++ final.py
@@ -127,7 +127,6 @@ st.markdown(
 # Row A
 col00 = st.columns(1)
 st.markdown('### Metrics: '+ df_city_area)
-col00.markdown( unsafe_allow_html=True)
 
 col0,col1, col2,col3, col4 = st.columns(5)
 metric_html0 = f"<div style='font-size: 24px; font-weight: bold;'>Average Price:</div>"
