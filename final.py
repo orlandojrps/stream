65ac74bcc78e24c0996833d9c4ed92fa387f69c6
Orlando Rocha dos Santos Junior <61721638+orlandojrps@users.noreply.github.com>
2023-03-06 15:51:30 +0000

    Update final.py

--- final.py
+++ final.py
@@ -536,9 +536,9 @@ df_filtered_pred['Average Price by City Area'] = df_filtered_pred['city_area'].m
 priceArea = df_filtered_pred['city_area'].map(average_price)
     
 #teste = median_price_m2.astype('int')
-m2 = int(median_price_m2.values[0])
-avg_m2=int(mean_price_m2.values[0])
-avg_m2_area = int(average_price.values[0])
+m2 = float(median_price_m2.values[0])
+avg_m2=float(mean_price_m2.values[0])
+avg_m2_area = float(average_price.values[0])
 
 features = np.array([pred_beds, pred_baths, pred_area, m2, avg_m2, avg_m2_area]).reshape(1, -1)
     
@@ -548,4 +548,4 @@ features = np.array([pred_beds, pred_baths, pred_area, m2, avg_m2, avg_m2_area])
 #######features = np.array([pred_beds, pred_baths, pred_area, pred_area, pred_area, pred_area]).reshape(1, -1)
 prediction = predict(features)
 #c2.write('Your Suggested Price is:', 12)  
-c2.markdown(f"<div style='font-size: 64px;'>{pred_beds, pred_baths, pred_area, m2, avg_m2, avg_m2_area}</div>", unsafe_allow_html=True)
+c2.markdown(f"<div style='font-size: 64px;'>{pred_beds, pred_baths, pred_area, m2, avg_m2, avg_m2_area,.2f}</div>", unsafe_allow_html=True)
